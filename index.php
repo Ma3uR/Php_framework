9a7e66958995477eda531dc64b72a579586d89ac
Andrey Mazurenko <AndreyMazur99@gmail.com>
2020-05-22 19:40:26 +0300

    MVC and rooting

--- index.php
+++ index.php
@@ -2,16 +2,11 @@
 if (file_exists('vendor/autoload.php')) {
     require ('vendor/autoload.php');
 }
-//require('config/config.php');
-//if (isset($_SERVER['PATH_INFO']) && ($controllerName = trim($_SERVER['PATH_INFO'], '/'))) {
-//    $controllerClass = "\App\Controller\\" . ucfirst($controllerName);
-//} else {
-//    $controllerClass = \App\Controller\Movies::class;
-//}
-//$controller = new $controllerClass;
-//$controller->execute();
-$url = $_SERVER['REQUEST_URI'];
-$url = explode('?', $url);
-$url = $url[0];
-
-echo $url;
+    require('config/config.php');
+    if (isset($_SERVER['REQUEST_URI']) && ($controllerName = trim($_SERVER['REQUEST_URI'], '/'))) {
+        $controllerClass = "\App\Controller\\" . ucfirst($controllerName);
+    } else {
+        $controllerClass = \App\Controller\Movies::class;
+    }
+$controller = new $controllerClass;
+$controller->execute();
